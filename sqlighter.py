d5272870caccf856d1a458b44d79c5a4638d4578
Kamilgeek <kamil-magomedov-00@mail.ru>
2020-09-29 17:07:29 +0400

    did method which add new subscriber in database

--- sqlighter.py
+++ sqlighter.py
@@ -13,6 +13,13 @@ class SQLighter:
             return self.cursor.execute("SELECT * FROM 'subscriptions' WHERE 'status' = ?", (status)).fetchall()
 
 
-    def subscriber_exists(self, user_id, status):
+    def subscriber_exists(self, user_id):
+        """Проверяем есть ли уже юзер в базе"""
         with self.connection:
             return self.cursor.execute("SELECT * FROM 'subscriptions' WHERE 'user_id' = ?", (user_id)).fetchall()
+
+    def add_subscriber(self, user_id, status = True):
+        """Добавляем нового подписчика"""
+        with self.connection:
+            return self.cursor.execute("INSERT INTO 'subscriptions' ('user_id', 'status') VALUES (?,?)", (user_id, status))
+
