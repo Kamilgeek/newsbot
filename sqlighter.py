0a35b7fd29fbbc574952ae3faf094da0bce77539
Kamilgeek <kamil-magomedov-00@mail.ru>
2020-09-29 17:01:04 +0400

    did methods which check subscriber exist

--- sqlighter.py
+++ sqlighter.py
@@ -4,10 +4,15 @@ class SQLighter:
 
     def __init__(self, database_file):
         """Подключаемся к БД и сохраняем курсор соединения"""
-        self.connection = sqlite3.connect(database)
+        self.connection = sqlite3.connect(database_file)
         self.cursor = self.connection.cursor()
 
     def get_subscribtions(self, status = True):
         """Получаем всех активных подписчиков бота """
         with self.connection:
-            return self.cursor.execute("SELECT * FROM 'subscriptions'")
+            return self.cursor.execute("SELECT * FROM 'subscriptions' WHERE 'status' = ?", (status)).fetchall()
+
+
+    def subscriber_exists(self, user_id, status):
+        with self.connection:
+            return self.cursor.execute("SELECT * FROM 'subscriptions' WHERE 'user_id' = ?", (user_id)).fetchall()
